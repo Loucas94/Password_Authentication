0125d12fd961e63fc977f7487bf66c177f12deed
v0.0.1 <louka@LOUCASASUS>
2025-03-20 17:18:45 +0200

    added user and authentication classes

---
+++ Authentication.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Authentication
+{
+    public class Authentication
+    {
+        private Dictionary<string, User> users = new Dictionary<string, User>();
+        private const int MaxAttempts = 3;
+
+        public bool RegisterUser(string username, string password)
+        {
+            if (users.ContainsKey(username))
+            {
+                return false; // User already exists
+            }
+            users[username] = new User(password);
+            return true;
+        }
+
+        public string AuthenticateUser(string username, string password)
+        {
+            if (!users.ContainsKey(username))
+            {
+                return "User does not exist.";
+            }
+
+            User user = users[username];
+
+            if (user.FailedAttempts >= MaxAttempts)
+            {
+                return "Account locked due to too many failed attempts.";
+            }
+
+            if (user.VerifyPassword(password))
+            {
+                user.FailedAttempts = 0;
+                return "Authentication successful!";
+            }
+            else
+            {
+                user.FailedAttempts++;
+                return $"Authentication failed! {MaxAttempts - user.FailedAttempts} attempts left.";
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Password_Authentication
+{
+    public class User
+    {
+        public string Salt { get; }
+        public string HashedPassword { get; private set; }
+        public int FailedAttempts { get; set; }
+
+        public User(string password)
+        {
+            Salt = GenerateSalt();
+            HashedPassword = HashPassword(password, Salt);
+            FailedAttempts = 0;
+        }
+
+        private string GenerateSalt()
+        {
+            byte[] saltBytes = new byte[16];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(saltBytes);
+            }
+            return Convert.ToBase64String(saltBytes);
+        }
+
+        public static string HashPassword(string password, string salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] saltedPassword = Encoding.UTF8.GetBytes(salt + password);
+                byte[] hash = sha256.ComputeHash(saltedPassword);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        public bool VerifyPassword(string password)
+        {
+            return HashedPassword == HashPassword(password, Salt);
+        }
+    }
+}
